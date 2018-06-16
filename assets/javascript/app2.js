2d66f09f93491d770d81fd9c05b90a1eedc411a2
Kayla Goldner <kaylamariegoldner1@Kaylas-MacBook-Air.local>
2018-06-15 22:32:32 -0700

    final

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,30 +1,27 @@
 $(document).ready(function () {
 
-  //User clicks "Start Game" button to start 30 second timer
+  // User clicks "Start Game" button to start 30 second timer
   $("#StartGame").on("click", function () {
     alert("Game has started! You have 1 minute & 30 seconds!");
     startTimer();
     console.log("Start Game");
   });
 
-  //Timer countdown
+  // Timer countdown
 
   document.getElementById('timer').innerHTML =
     1 + ":" + 30;
-  
+
 
   function startTimer() {
-    var presentTime = document.getElementById('timer').innerHTML;
-    var timeArray = presentTime.split(/[:]+/);
-    var m = timeArray[0];
-    var s = checkSecond((timeArray[1] - 1));
-    if(s==59){m=m-1}
+    let presentTime = document.getElementById('timer').innerHTML;
+    let timeArray = presentTime.split(/[:]+/);
+    let m = timeArray[0];
+    let s = checkSecond((timeArray[1] - 1));
+    if (s == 59) { m = m - 1 }
     if (m < 0) {
       alert('You Are Out Of Time!')
       alert(' Submit & Check Your Answers!')
-
-
-      // showResults();
 
       return;
     }
@@ -38,18 +35,15 @@ $(document).ready(function () {
   function checkSecond(sec) {
     if (sec < 10 && sec >= 0) {
       sec = "0" + sec
-    }; // add zero in front of numbers < 10
+    };
     if (sec < 0) {
       sec = "59"
     };
     return sec;
   }
 
-  //////////////////////////////////////////////////
 
-
-
-////// Begin calculating correct vs false answers
+  // Begin calculating correct vs false answers
 
   function setScore() {
     let score = $('input:checked[value=correct]').length
@@ -68,23 +62,25 @@ $(document).ready(function () {
     $('#wrong').text(incorrectScore);
   }
 
-  // set score on input change
+  // Set score on input change
   $('input').change(setScore);
   $('input').change(correctScore);
   $('input').change(incorrectScore);
 
-  // set score on initialization
+  // Set score on initialization
   setScore();
 
   correctScore();
 
   incorrectScore();
 
-/// show scores
-$( "#button2" ).click(function() {
-  $( ".score" ).toggle();
-});
-  
-//
+  // Show scores
+  $("#button2").click(function () {
+    $(".score").toggle();
+    $("#correct").css("background-color", "green");
+    $("#wrong").css("background-color", "red");
+  });
+
+
 
 });
